3ab0329b01577cfc7b3b758a6d32e4f2fffdf3c5
aarya2000 <abarve@purdue.edu>
2018-09-28 13:12:55 -0400

    Appropriate changes made to the files.

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -7,6 +7,7 @@ public class StringUtils {
      * 7 characters. All should be lowercase.
      * Ex: a = "Sean ", b = "Flannery", returns "sflanner"
      */
+    
     public static String makePurdueUsername(String firstName, String lastName) {
         /* Generate the long version of the name first:
             Ex: firstName = "Sean", lastName = "Flannery"
